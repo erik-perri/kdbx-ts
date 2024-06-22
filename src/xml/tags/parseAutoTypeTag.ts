17607e007f430451b08c7fb65d08771b342759ed
Erik Perri <erik.perri@gmail.com>
2024-06-22 14:23:03 -0400

    refactor: merged auto type parser files

--- src/xml/tags/parseAutoTypeTag.ts
+++ src/xml/tags/parseAutoTypeTag.ts
@@ -1,6 +1,6 @@
 import type AutoType from '../../structure/AutoType';
+import type AutoTypeAssociation from '../../structure/AutoTypeAssociation';
 import type KdbxXmlReader from '../../utilities/KdbxXmlReader';
-import parseAutoTypeAssociationTag from './parseAutoTypeAssociationTag';
 
 export default function parseAutoTypeTag(reader: KdbxXmlReader): AutoType {
   reader.assertOpenedTagOf('AutoType');
@@ -39,4 +39,41 @@ export default function parseAutoTypeTag(reader: KdbxXmlReader): AutoType {
   }
 
   return autoType;
+}
+
+function parseAutoTypeAssociationTag(
+  reader: KdbxXmlReader,
+): AutoTypeAssociation {
+  reader.assertOpenedTagOf('Association');
+
+  const association: Partial<AutoTypeAssociation> = {};
+
+  while (reader.readNextStartElement()) {
+    switch (reader.current.name) {
+      case 'Window':
+        association.window = reader.readStringValue();
+        break;
+
+      case 'KeystrokeSequence':
+        association.sequence = reader.readStringValue();
+        break;
+
+      default:
+        throw new Error(
+          `Unexpected tag "${reader.current.name}" while parsing "Association"`,
+        );
+    }
+  }
+
+  if (!isAutoTypeAssociationComplete(association)) {
+    throw new Error('Found "Association" tag with incomplete data');
+  }
+
+  return association;
+}
+
+function isAutoTypeAssociationComplete(
+  association: Partial<AutoTypeAssociation>,
+): association is AutoTypeAssociation {
+  return association.window !== undefined && association.sequence !== undefined;
 }
