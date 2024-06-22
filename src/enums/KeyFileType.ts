cf17be966775d33433e6a8b88b709e4f2294b7d8
Erik Perri <erik.perri@gmail.com>
2024-06-22 19:15:05 -0400

    refactor: removed unimplemented enum values

--- src/enums/KeyFileType.ts
+++ src/enums/KeyFileType.ts
@@ -1,10 +1,5 @@
 const FileKeyType = Object.freeze({
-  None: 0,
   Hashed: 1,
-  KeePass2XML: 2,
-  KeePass2XMLv2: 3,
-  FixedBinary: 4,
-  FixedBinaryHex: 5,
 } as const);
 
 type FileKeyType = (typeof FileKeyType)[keyof typeof FileKeyType];
