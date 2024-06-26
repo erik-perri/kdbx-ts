cbc5cce018e39be7e581b011f83167aa6b65b06d
Erik Perri <erik.perri@gmail.com>
2024-06-26 18:34:29 -0400

    refactor: simplified uuid display

--- src/utilities/displayUuid.ts
+++ src/utilities/displayUuid.ts
@@ -5,19 +5,8 @@ export default function displayUuid(uuid: Uint8Array): string {
     );
   }
 
-  const uuidAsHex: string = [...uuid]
-    .map((input: number) => input.toString(16).padStart(2, '0'))
-    .join('');
-
-  return (
-    uuidAsHex.slice(0, 8) +
-    '-' +
-    uuidAsHex.slice(8, 12) +
-    '-' +
-    uuidAsHex.slice(12, 16) +
-    '-' +
-    uuidAsHex.slice(16, 20) +
-    '-' +
-    uuidAsHex.slice(20)
-  ).toLowerCase();
+  return Buffer.from(uuid)
+    .toString('hex')
+    .replace(/^(.{8})(.{4})(.{4})(.{4})(.{12})$/, '$1-$2-$3-$4-$5')
+    .toLowerCase();
 }
