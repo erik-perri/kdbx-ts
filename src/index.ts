4a7b8e062ba665be617f75b2ade2f0565c162efb
Erik Perri <erik.perri@gmail.com>
2024-06-25 22:43:38 -0400

    update: added more exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
 /* v8 ignore start */
+import Argon2Type from './enums/Argon2Type';
+import Argon2Version from './enums/Argon2Version';
+import CompressionAlgorithm from './enums/CompressionAlgorithm';
+import DefaultIconNumber from './enums/DefaultIconNumber';
+import HashAlgorithm from './enums/HashAlgorithm';
+import KdfUuid from './enums/KdfUuid';
+import SymmetricCipherAlgorithm from './enums/SymmetricCipherAlgorithm';
+import SymmetricCipherDirection from './enums/SymmetricCipherDirection';
+import SymmetricCipherUuid from './enums/SymmetricCipherUuid';
+import createChallengeResponseKey from './keys/createChallengeResponseKey';
+import createFileKey from './keys/createFileKey';
+import createPasswordKey from './keys/createPasswordKey';
+import randomizeSeeds from './randomizeSeeds';
 import readDatabase from './readDatabase';
+import type { CryptoCipher, CryptoImplementation } from './types/crypto';
+import type {
+  KdbxChallengeResponseKey,
+  KdbxFileKey,
+  KdbxKey,
+  KdbxPasswordKey,
+  KdbxProcessedKey,
+} from './types/keys';
+import writeDatabase from './writeDatabase';
 
-export { readDatabase };
+export type { CryptoCipher, CryptoImplementation };
+export type {
+  KdbxChallengeResponseKey,
+  KdbxFileKey,
+  KdbxKey,
+  KdbxPasswordKey,
+  KdbxProcessedKey,
+};
+export {
+  Argon2Type,
+  Argon2Version,
+  CompressionAlgorithm,
+  DefaultIconNumber,
+  HashAlgorithm,
+  KdfUuid,
+  SymmetricCipherAlgorithm,
+  SymmetricCipherDirection,
+  SymmetricCipherUuid,
+};
+export {
+  createChallengeResponseKey,
+  createFileKey,
+  createPasswordKey,
+  randomizeSeeds,
+  readDatabase,
+  writeDatabase,
+};
 /* v8 ignore stop */
