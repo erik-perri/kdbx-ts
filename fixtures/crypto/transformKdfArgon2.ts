d83d980a9068bf9954826362bfc816dbf0230ce0
Erik Perri <erik.perri@gmail.com>
2024-06-27 22:49:30 -0400

    refactor: moved transform to function for bundle testing

---
+++ fixtures/crypto/transformKdfArgon2.ts
@@ -0,0 +1,20 @@
+import * as argon2 from 'argon2';
+
+import { type KdbxArgon2KdfParameters } from '../../src/types/format';
+
+export default async function transformKdfArgon2(
+  key: Uint8Array,
+  parameters: KdbxArgon2KdfParameters,
+): Promise<Uint8Array> {
+  return Uint8Array.from(
+    await argon2.hash(Buffer.from(key), {
+      memoryCost: Number(parameters.memoryInBytes / BigInt(1024)),
+      parallelism: Number(parameters.parallelism),
+      salt: parameters.seed,
+      timeCost: Number(parameters.iterations),
+      type: parameters.type,
+      version: parameters.version,
+      raw: true,
+    }),
+  );
+}
--- fixtures/vitest.setup.ts
+++ fixtures/vitest.setup.ts
@@ -1,23 +1,10 @@
-import * as argon2 from 'argon2';
-
 import { configureDependencies } from '../src/dependencies';
 import createChaCha20Cipher from './crypto/createChaCha20Cipher';
 import createTwofishCbcCipher from './crypto/createTwofishCbcCipher';
+import transformKdfArgon2 from './crypto/transformKdfArgon2';
 
 configureDependencies({
   cipherChaCha20: createChaCha20Cipher,
   cipherTwofish: createTwofishCbcCipher,
-  transformKdfArgon2: async (key, parameters) => {
-    return Uint8Array.from(
-      await argon2.hash(Buffer.from(key), {
-        memoryCost: Number(parameters.memoryInBytes / BigInt(1024)),
-        parallelism: Number(parameters.parallelism),
-        salt: parameters.seed,
-        timeCost: Number(parameters.iterations),
-        type: parameters.type,
-        version: parameters.version,
-        raw: true,
-      }),
-    );
-  },
+  transformKdfArgon2: transformKdfArgon2,
 });
