c3c1548b49ecebbd1409046310d3e34b76ccb893
Erik Perri <erik.perri@gmail.com>
2024-08-03 20:54:11 -0400

    fix: rounded timestamp

--- src/utilities/gregorianTimestampFromDate.ts
+++ src/utilities/gregorianTimestampFromDate.ts
@@ -2,7 +2,7 @@ export default function gregorianTimestampFromDate(date: Date): number {
   const julianSecondsFrom1970To0001 = 62135596800;
 
   const unixTimestamp = date.getTime();
-  const unixSeconds = unixTimestamp / 1000;
+  const unixSeconds = Math.round(unixTimestamp / 1000);
 
   return unixSeconds + julianSecondsFrom1970To0001;
 }
