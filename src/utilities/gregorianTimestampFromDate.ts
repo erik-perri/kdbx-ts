095e189c09e8c792e0104a5fb6396c7282aed159
Erik Perri <erik.perri@gmail.com>
2025-03-22 23:32:50 -0400

    refactor: rename variable

--- src/utilities/gregorianTimestampFromDate.ts
+++ src/utilities/gregorianTimestampFromDate.ts
@@ -1,8 +1,8 @@
 export default function gregorianTimestampFromDate(date: Date): number {
-  const julianSecondsFrom1970To0001 = 62135596800;
+  const gregorianSecondsFrom1970To0001 = 62135596800;
 
   const unixTimestamp = date.getTime();
   const unixSeconds = Math.round(unixTimestamp / 1000);
 
-  return unixSeconds + julianSecondsFrom1970To0001;
+  return unixSeconds + gregorianSecondsFrom1970To0001;
 }
